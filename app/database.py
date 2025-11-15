835831e28ce1063e8fcef4d9c0a87f24b58dc0ed
mkmovieshu <ipsappdevelopers@gmail.com>
2025-11-15 22:21:43 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,23 +1,9 @@
-# app/database.py
-import motor.motor_asyncio
-from .config import MONGO_URI, DB_NAME
-import logging
+from motor.motor_asyncio import AsyncIOMotorClient
+from app.config import MONGO_URL
 
-log = logging.getLogger("video-web.db")
+client = AsyncIOMotorClient(MONGO_URL)
+db = client["angel"]
 
-client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
-db = client[DB_NAME]
-
-# collections
-users_col = db["users"]
-videos_col = db["videos"]
-ad_col = db["ad_sessions"]
-
-async def ensure_indexes():
-    try:
-        await videos_col.create_index([("file_id",1)], unique=False)
-        await videos_col.create_index([("channel_id",1),("message_id",1)], unique=True, sparse=True)
-        await users_col.create_index([("user_id",1)], unique=True)
-        await ad_col.create_index([("token",1)], unique=True)
-    except Exception:
-        log.exception("ensure_indexes failed")
+users = db["users"]
+videos = db["videos"]
+ad_sessions = db["ad_sessions"]
