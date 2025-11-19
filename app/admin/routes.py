e491fd6de703fae385bbc275019481afdbd4cb7d
mkmovieshu <ipsappdevelopers@gmail.com>
2025-11-19 22:21:20 +0530

    Delete app directory

--- app/admin/routes.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from app.database import videos
-
-admin_router = APIRouter(prefix="/admin")
-
-@admin_router.post("/add_video")
-async def add_video(file_id: str):
-    await videos.insert_one({"file_id": file_id})
-    return {"status": "added"}
--- app/database.py
+++
@@ -1,9 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from app.config import MONGO_URL
-
-client = AsyncIOMotorClient(MONGO_URL)
-db = client["angel"]
-
-users = db["users"]
-videos = db["videos"]
-ad_sessions = db["ad_sessions"]
