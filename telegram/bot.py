418da75e2f83a44ba30b2dd3ea99d2be94effec4
mkmovieshu <ipsappdevelopers@gmail.com>
2025-11-19 22:27:28 +0530

    Create bot.py

---
+++ telegram/bot.py
@@ -0,0 +1,26 @@
+import asyncio
+import os
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    CallbackQueryHandler
+)
+from app.telegram.handlers import start_handler, next_handler
+from app.telegram.handlers_ad import handle_ad_check
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+async def run():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start_handler))
+    app.add_handler(CallbackQueryHandler(next_handler, pattern="^next"))
+    app.add_handler(CallbackQueryHandler(handle_ad_check, pattern="^ad_check"))
+
+    await app.initialize()
+    await app.start()
+    await app.updater.start_polling()
+    await app.updater.idle()
+
+if __name__ == "__main__":
+    asyncio.run(run())
